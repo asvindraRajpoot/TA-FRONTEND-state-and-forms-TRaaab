31894b127f1007b0c0e4ed3f5ebacdb93dc79569
asvindra <aswin1399904@gmail.com>
2021-11-27 10:36:23 +0530

    completed First part of form validation using react

---
+++ block-BRaabr/form-validation/src/Components/Form2.js
@@ -0,0 +1,22 @@
+import React from  "react";
+
+
+class Form2 extends React.Component{
+    constructor(props){
+        super(props);
+    }
+
+
+    render(){
+        return (
+            <>
+
+              <h1></h1>
+            </>
+        )
+
+    }
+
+}
+
+export default Form2;
