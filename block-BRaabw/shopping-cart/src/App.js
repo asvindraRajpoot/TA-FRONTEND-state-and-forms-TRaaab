e1c8d713a37800d8550e573ddf94b186a45ebf2a
asvindra <aswin1399904@gmail.com>
2021-11-29 12:01:30 +0530

    completed Shopping cart

---
+++ block-BRaabw/shopping-cart/src/App.js
@@ -0,0 +1,16 @@
+import Header from './components/Header';
+import Main from './components/Main';
+import './stylesheet/style.css';
+
+
+function App() {
+  return (
+    <>
+     <Header />
+     <Main/>
+    </>
+
+  );
+}
+
+export default App;
